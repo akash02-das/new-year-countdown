1686272fc232fc406aff05b5b89b0ec16a0c1fdf
akash <aku.das02@gmail.com>
2020-08-10 14:39:41 +0600

    dynamic year and spinner

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set Background Year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
     const currentTime = new Date();
     const diff = newYearTime - currentTime;
@@ -16,6 +22,7 @@ function updateCountdown() {
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
+    // Add values to DOM
     days.innerHTML = d;
     hours.innerHTML = h < 10 ? '0' + h : h;
     minutes.innerHTML = m < 10 ? '0' + m : m;
@@ -24,4 +31,11 @@ function updateCountdown() {
 
 }
 
+// Show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
 setInterval(updateCountdown, 1000);
